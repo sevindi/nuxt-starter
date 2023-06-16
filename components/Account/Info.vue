f0176041657b9dfbbe088a5e97d4ddfa332d3479
Samet Sevindi <samet.sevindi@gmail.com>
2023-06-16 19:29:21 +0300

    Initial commit

---
+++ components/Account/Info.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-if="user" class="flex items-center gap-3">
+        <S3Image :src="user.picture" class="w-8 h-8 object-contain rounded-full ring-2" />
+
+        <div class="flex flex-col text-base md:text-sm">
+            <NText strong>{{ user.name }}</NText>
+            <NText :depth="3">{{ user.email }}</NText>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { useUser } = useAuthSession()
+const user = useUser()
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <Navbar></Navbar>
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({ auth: false, middleware: "guest", colorMode: "light" });
+</script>
